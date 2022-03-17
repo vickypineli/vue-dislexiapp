36cf33e60a9b9969f71ba252e8fc8cdc51ae9bb9
vickypineli <v.pinero70@gmail.com>
2022-03-17 19:53:54 +0100

    .

--- back/test/activities/test_activities.py
+++ back/test/activities/test_activities.py
@@ -3,7 +3,7 @@ from src.lib.utils import temp_file
 from src.webserver import create_app
 from src.domain.activities import ActivityRepository, Activity
 
-def test_should_return_activities():
+def test_should_return_all_activities():
     activity_repository = ActivityRepository(temp_file())
     app = create_app(repositories={"activities": activity_repository})
     client = app.test_client()
