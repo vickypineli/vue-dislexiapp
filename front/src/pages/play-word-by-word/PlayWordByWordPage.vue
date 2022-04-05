bad38060e16ec6f4123c9264fb2d4985acb6ab5e
vicky <v.pinero70@gmail.com>
2022-04-05 15:02:51 +0200

    play

--- front/src/pages/play-word-by-word/PlayWordByWordPage.vue
+++ front/src/pages/play-word-by-word/PlayWordByWordPage.vue
@@ -1,68 +1,69 @@
 <template>
   <h1>IRAKURTZAILE</h1>
   <section id="textreader">
-      <div id="output" v-for="word in textByWords" :key="word">{{word}}</div>
-      <br>
-      <button @click="StartText();">PLAY</button>
-      <button @click="PauseText();">PAUSE</button>
+    <button @click="StartText()">PLAY</button>
+    <button @click="PauseText()">PAUSE</button>  
+    <div id="output">
+      <h1>{{ word }}</h1>
+    </div>
   </section>
-  {{word}}
 </template>
 
 <script>
 export default {
-    name: 'PlayText',
-    data() {
-        return {
-            text: "I can't believe the news today. Oh I can't close my eyes and make it go away",
-            word:"",
-            timeInterval: 1*1000,
-            textByWords:[],
-            play: null,
-        }
+  name: "PlayText",
+  data() {
+    return {
+      text: "I can't believe the news today. Oh I can't close my eyes and make it go away",
+      word: "",
+      timeInterval: 10 * 100,
+      textByWords: [],
+      play: 0,
+    };
+  },
+  mounted() {
+   
+  },
+  methods: {
+    PauseText() {
+      this.pause = clearInterval(this.play);
     },
-    mounted() {
-        this.textInDisplay()
+    StartText() {
+        this.textByWords = this.text.split(" ");
+        let item = 0;
+        this.play = setInterval(() => {
+        this.word = this.textByWords[item];
+        item += 1;
+            }, this.timeInterval);
     },
-    methods: { 
-        textInDisplay(){
-            this.textByWords = this.text.split(" ");                             
-        },
-        PauseText() {
-            this.pause = clearInterval(this.play);
-        },
-        StartText(){
-            this.play = setInterval(() =>{this.word++},this.timeInterval);
-            {{this.word++}}
-            if(this.word >= this.textByWords.length -1){       
-                clearInterval(this.play);
-            }else{
-                this.word++; 
-            }
-        }
-    }
+  },
 };
-
 </script>
 
 <style>
 body {
-      text-align: center;
-    }
-    section{
-      margin: 1.5em;
-      border: 0.3em solid red;
-      padding: 1.5em;
-    }
-    button{
-        color:blue;
-        font-size: 1.5em;
-    }
-    h1{
-        color:green;
-        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  text-align: center;
+}
+section {
+  margin: 1.5em;
+  border: 0.3em solid red;
+  padding: 1.5em;
+}
+button {
+  color: blue;
+  font-size: 1.5em;
+}
+h1 {
+  color: green;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  
+}
 
-    }
+#output{
+    height: 2em;
+    font-size: 3em;
+
+}
 </style>
 
     
--- front/src/pages/word-by-word/WordByWordPage.vue
+++ front/src/pages/word-by-word/WordByWordPage.vue
@@ -1,56 +1,82 @@
 <template>
-    <h1>HITZEZ HITZ</h1>
-      <article>
-          <input type="radio" id="ingles" value="english_text" v-model="lenguageselected">
-          <label for="uno">Inglesa</label>
-          
-          <input type="radio" id="euskera" value="basque_text" v-model="lenguageselected">
-          <label for="Dos">Euskera</label>
+  <h1>HITZEZ HITZ</h1>
+  <article>
+    <input
+      type="radio"
+      id="ingles"
+      value="english_text"
+      v-model="lenguageselected"
+    />
+    <label for="uno">Inglesa</label>
 
-          <input type="radio" id="text.text" value="spanish_text" v-model="lenguageselected">
-          <label for="Gazte">Gaztelania</label>
-          <br>
-          <span>{{ lenguageselected }}</span>
-          <br>
-      </article>
-      <article>
-          <!-- <button @click.prevent="">EUSKERA</button>
+    <input
+      type="radio"
+      id="euskera"
+      value="basque_text"
+      v-model="lenguageselected"
+    />
+    <label for="Dos">Euskera</label>
+
+    <input
+      type="radio"
+      id="text.text"
+      value="spanish_text"
+      v-model="lenguageselected"
+    />
+    <label for="Gazte">Gaztelania</label>
+    <br />
+    <span>{{ lenguageselected }}</span>
+    {{ texts }}
+    <br />
+  </article>
+  <article>
+    <!-- <button @click.prevent="">EUSKERA</button>
           <button @click.prevent="">INGELESA</button>
           <button @click.prevent="">GAZTELANIA</button> -->
-          <!-- {{text.text}} -->
-      </article>    
-      <article>
-        <label>Hitz-minuturo</label>
-        <input v-model="wordsperminute" type="number" min="30" max="100">
-        <br>
-        <span> {{wordsperminute}}</span>
-      <article>
-        <br>
-      </article>
-        <label>Letra-tipo</label>
-        <select v-model="fontselected">
-          <option disabled value="letra-tipo">Select</option>
-          <option value ="arial">Arial</option>
-          <option value ="escolar">Escolar</option>
-          <option value ="dislexia">Dislexia</option>
-        </select>
-        <br>
-        <span> {{ fontselected }}</span>
-      </article>
-      <article>
-        <label for="checkbox">Entzun: {{ voiceselected }}</label>
-        <input type="checkbox" id="checkbox" true-value="BAI" false-value="EZ" v-model="voiceselected">
-      </article>
-      <article>
-        <input type="submit" value="Datuak gorde">
-      </article>
-    <article class="text-list">
-          <li class="text-item" v-for="text in texts" :key="text">{{text.text}}</li>
-      <ul>
-          <li class="text-item" v-for="text, index in texts" :key="index">{{text.language}}</li>
-      </ul>
-    </article>    
-    <router-link to="/activities/word-by-word/play-word-by-word"><button>GOAZEN IRAKURTZERA..!!</button></router-link>
+    <!-- {{text.text}} -->
+  </article>
+  <article>
+    <label>Hitz-minuturo</label>
+    <input v-model="wordsperminute" type="number" min="30" max="100" />
+    <br />
+    <span> {{ wordsperminute }}</span>
+    <article>
+      <br />
+    </article>
+    <label>Letra-tipo</label>
+    <select v-model="fontselected">
+      <option disabled value="letra-tipo">Select</option>
+      <option value="arial">Arial</option>
+      <option value="escolar">Escolar</option>
+      <option value="dislexia">Dislexia</option>
+    </select>
+    <br />
+    <span> {{ fontselected }}</span>
+  </article>
+  <article>
+    <label for="checkbox">Entzun: {{ voiceselected }}</label>
+    <input
+      type="checkbox"
+      id="checkbox"
+      true-value="BAI"
+      false-value="EZ"
+      v-model="voiceselected"
+    />
+  </article>
+  <article>
+    <input type="submit" value="Datuak gorde" />
+  </article>
+  <article class="text-list">
+    <li class="text-item" v-for="text in texts" :key="text">{{ text.text }}</li>
+    <ul>
+      <li class="text-item" v-for="(text, index) in texts" :key="index">
+        {{ text.language }}
+      </li>
+    </ul>
+  </article>
+  <router-link to="/activities/word-by-word/play-word-by-word"
+    ><button>GOAZEN IRAKURTZERA..!!</button></router-link
+  >
 </template>"
 <script>
 export default {
@@ -62,6 +88,7 @@ export default {
           wordsperminute:'',
           voiceselected:'',
           texts:[],
+          actualtext:"",
         }
     },
     mounted(){
@@ -72,51 +99,54 @@ export default {
         const response = await fetch('http://localhost:5000/api/activities/wordbyword')
         this.texts = await response.json()
       },
-      async textselected(){
-        const response = await fetch('http://localhost:5000/api/activities/wordbyword/<language>')
-        this.text = await response.json()
-      },
+      textselected(){
+        this.texts.filter((text)=>text.language==this.lenguageselected)
+      }
     }    
 }
 </script>
 
 <style>
-p{
+p {
   font-size: 1.5em;
 }
 
-li{
+li {
   text-align: left;
-  margin-left:35.5px;
+  margin-left: 35.5px;
 }
-form{
+form {
   border: 0.5em solid gray;
   background: rgb(179, 176, 176);
 }
-label, get-text {
+label,
+get-text {
   padding: 1em;
   font-size: 1.5em;
 }
-button, input, select, span {
-  font-size:1.5em;
-  color:green;
+button,
+input,
+select,
+span {
+  font-size: 1.5em;
+  color: green;
 }
-li{
+li {
   font-size: 1 em;
 }
-article{
+article {
   background: rgb(204, 243, 198);
 }
-.ingles{
+.ingles {
   font-family: arial;
   color: blue;
 }
-.euskera{
+.euskera {
   font-family: escolar;
   color: red;
 }
-.ingles{
+.ingles {
   font-family: dislexia;
-  color:green;
+  color: green;
 }
 </style>
