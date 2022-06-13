593a1eda7d693600e0e34957cd6f11089e212d1b
vickypineli <v.pinero70@gmail.com>
2022-06-13 18:51:42 +0200

    cambio a github

--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -16,7 +16,6 @@ def main():
     info_repository.save(Info(app_name="irla-kurri"))
 
     #Wordbyword
-
     textoriginal_1 = Wordbyword (language ="Gaztelania", text = "En un lugar de la Mancha de cuyo nombre no quiero acordarme, no hace mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. ")
     textoriginal_2 = Wordbyword (language ="Ingelesa", text = "I can't believe the news today Oh, I can't close my eyes and make it go away. How long must we sing this song?")
     textoriginal_3 = Wordbyword (language ="Euskera", text = "Hala bazan ala ez bazan, sar dadila kalabazan eta atera dadila Foruko plazan ")
@@ -28,7 +27,6 @@ def main():
     wordbyword_repository.save(textoriginal_3)
 
     # Activities
-
     Activity_1 = Activity (id="act-1", user_id="user-1", name = "HITZEZ HITZ")
     Activity_2 = Activity (id="act-2", user_id="user-2", name = "IRAKUR-LAGUN")
     Activity_3 = Activity (id="act-3", user_id="user-1", name = "SILABAK ZENBATU")
@@ -80,5 +78,4 @@ def main():
     countletters_repository.save(word10)
 
     print("Base de datos inicializada en" + database_path)
-
 main()
